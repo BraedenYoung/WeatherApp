61daf570d9237e95ff18275675f11c327906b601
Braeden Young <braeden.w.young@gmail.com>
2015-07-12 21:01:30 -0400

    Wx-app : Add refresh button to retrieve current weather information

--- WxApp/app/src/main/java/com/app/braedenyoung/wxapp/MainActivity.java
+++ WxApp/app/src/main/java/com/app/braedenyoung/wxapp/MainActivity.java
@@ -1,17 +1,15 @@
 package com.app.braedenyoung.wxapp;
 
 import android.app.Activity;
-import android.app.DownloadManager;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
-import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,6 +40,8 @@ public class MainActivity extends Activity {
     @InjectView(R.id.precipValue) TextView mPrecipValue;
     @InjectView(R.id.summaryLabel) TextView mSummaryLabel;
     @InjectView(R.id.iconImageView) ImageView mIconImageView;
+    @InjectView(R.id.refreshImageView) ImageView mRefreshImageView;
+    @InjectView(R.id.progressBar) ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +50,34 @@ public class MainActivity extends Activity {
 
         ButterKnife.inject(this);
 
+        mProgressBar.setVisibility(View.INVISIBLE);
+
+        final double latitude = 37.8267;
+        final double longitude = -122.423;
+
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getForecast(latitude, longitude);
+            }
+        });
+
+        getForecast(latitude, longitude);
+    }
+
+    private void getForecast(double latitude, double longitude) {
         String apiKey = "8068c6d598ecdcdb3e7232785ff25c20";
-        double latitude = 37.8267;
-        double longitude = -122.423;
+
         String forecastUrl = "https://api.forecast.io/forecast/"+apiKey+"/"+latitude+","+longitude;
 
         if (isNetworkAvailable()) {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    toggleRefresh();
+                }
+            });
 
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
@@ -67,10 +89,26 @@ public class MainActivity extends Activity {
                 @Override
                 public void onFailure(Request request, IOException e) {
 
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggleRefresh();
+                        }
+                    });
+                    alertUserAboutError();
+
                 }
 
                 @Override
                 public void onResponse(Response response) throws IOException {
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggleRefresh();
+                        }
+                    });
+
                     try {
                         String jsonData = response.body().string();
                         Log.v(TAG, jsonData);
@@ -78,8 +116,7 @@ public class MainActivity extends Activity {
 
                             mCurrentWeather = getCurrentDetails(jsonData);
 
-                            TextView temperatureTextView = (TextView)findViewById(R.id.temperatureLabel);
-//                          temperatureTextView.setText(String.valueOf(mCurrentWeather.getTemperature()));
+                            TextView temperatureTextView = (TextView) findViewById(R.id.temperatureLabel);
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -103,9 +140,20 @@ public class MainActivity extends Activity {
         }
     }
 
+    private void toggleRefresh() {
+        if (mProgressBar.getVisibility() == View.INVISIBLE) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRefreshImageView.setVisibility(View.INVISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRefreshImageView.setVisibility(View.VISIBLE);
+        }
+
+    }
+
     private void updateDisplay() {
         mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
-        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + "it will be");
+        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it will be");
         mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
         mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
         mSummaryLabel.setText(mCurrentWeather.getSummary());
